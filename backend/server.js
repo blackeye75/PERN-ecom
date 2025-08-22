727a324db3ffc9643475b54b2561f04a37e34410
blackeye75 <priyanshuraj22275@gmail.com>
2025-08-22 23:24:52 +0530

    initialize server setup with express and middleware

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ async function initDB(){
   try {
     
   } catch (error) {
-    console.log(`Error initializing database: ${error.message}`);
+    console.log(`Error initializing databa: ${error.message}`);
     
   }
 }
